24aadb94f2f155df8fdb41e2aaffd002a08a4cb7
Sumita2611 <jhasumita3@gmail.com>
2026-01-19 19:49:19 +0530

    proceeding with learning shell scripting

---
+++ 09_arith_ops.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Maths calculation
+x=10
+y=2
+
+let mul=$x*$y
+echo "$mul"
+
+echo "Subtraction is $(($x-$y))"
---
+++ 15_forloop1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in {1..5}
+do 
+	echo "Number is $i"
+done
+
+for name in Raju Sham Ram
+do 
+	echo "Name is $name"
+done
